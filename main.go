c7bc2039f51ff551bf49d2efb36f9b22492b0013
johnnliu <johnnliu@tencent.com>
2022-01-10 01:48:23 +0800

    添加配置读取模块

--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"gohub/bootstrap"
+	"gohub/pkg/config"
 
 	"github.com/gin-gonic/gin"
+
+	btsConfig "gohub/config"
 )
 
+func init() {
+	btsConfig.Initialize()
+}
+
 func main() {
+	// 配置初始化，依赖命令行 --env 参数
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
+
 	r := gin.New()
 
 	// 注册中间件
@@ -14,5 +29,9 @@ func main() {
 
 	bootstrap.SetRoute(r)
 
-	r.Run(":8888")
+	err := r.Run(":" + config.Get("app.port"))
+	if err != nil {
+		// 错误处理，端口被占用了或者其他错误
+		fmt.Println(err.Error())
+	}
 }
